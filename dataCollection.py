eaf4e71ce49da2dd46f666d7c6bea1fffd01acce
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2024-04-08 01:37:32 +0530

    A dataset + DataCollection(Not working)

--- dataCollection.py
+++ dataCollection.py
@@ -8,7 +8,7 @@ offset = 20
 imgSize = 300
 counter = 0
 
-folder = "Data/B"
+folder = "Data/A"
 
 cap = cv2.VideoCapture(0)  # Index of Camera being used
 
@@ -24,7 +24,7 @@ while True:
             x, y, w, h = hand["bbox"]  # get value of bounding box dimensions
 
             imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-            imgCrop = img[y - offset : y + h, x - offset : x + w]  # cropped image size
+            imgCrop = img[y - offset : y + h + 25, x - offset : x + w + 25]  # cropped image size
 
             imgCropShape = imgCrop.shape
 
